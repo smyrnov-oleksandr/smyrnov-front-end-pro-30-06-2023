40c918c99d52bf0d598660c6fca10b16a0306b71
[oleksandr_smyrnov] <[oleksandrsmyrnov.work@gmail.com]git config --global user.email [oleksandrsmyrnov.work@gmail.com]>
2023-07-06 21:09:25 +0200

    unit_3.string

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
+// let userName = prompt('What is your name?');
+// alert(`Hello, ${userName}! How are you?`);
+
 let userName = prompt('What is your name?');
-alert(`Hello, ${userName}! How are you?`);
+let userAge = prompt('How old are you?');
+let userLocation = prompt('Where are you from?');
+
+alert(userName + ' ' + userAge + ' ' + userLocation);
+alert(`Your name: ${userName}
+Your age: ${userAge}
+Your city: ${userLocation}`);
