0e62b506ecf398d9a892f7a0e1ccc102bc12537f
[oleksandr_smyrnov] <[oleksandrsmyrnov.work@gmail.com]git config --global user.email [oleksandrsmyrnov.work@gmail.com]>
2023-07-11 20:12:12 +0200

    Add unit_5(Arithmetic mean)

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 // let userName = prompt('What is your name?');
 // alert(`Hello, ${userName}! How are you?`);
 
-let userName = prompt('What is your name?');
-let userAge = prompt('How old are you?');
-let userLocation = prompt('Where are you from?');
+// let userName = prompt('What is your name?');
+// let userAge = prompt('How old are you?');
+// let userLocation = prompt('Where are you from?');
 
-alert(userName + ' ' + userAge + ' ' + userLocation);
-alert(`Your name: ${userName}
-Your age: ${userAge}
-Your city: ${userLocation}`);
+// alert(userName + ' ' + userAge + ' ' + userLocation);
+// alert(`Your name: ${userName}
+// Your age: ${userAge}
+// Your city: ${userLocation}`);
+
+// UNIT_5
+let num1 = +prompt('Your first number is:');
+let num2 = +prompt('Your second number is:');
+let num3 = +prompt('Your third number is:');
+let res = alert(`Arithmetic mean: ${(num1 + num2 + num3) / 3}`);
