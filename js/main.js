75d48906048595df16c64a81f7a34dc5c11614cc
[oleksandr_smyrnov] <[oleksandrsmyrnov.work@gmail.com]git config --global user.email [oleksandrsmyrnov.work@gmail.com]>
2023-07-11 20:03:18 +0200

    Add unit_4(hours to seconds)

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
+// UNIT_1
 // let userName = prompt('What is your name?');
 // alert(`Hello, ${userName}! How are you?`);
 
-let userName = prompt('What is your name?');
-let userAge = prompt('How old are you?');
-let userLocation = prompt('Where are you from?');
+// UNIT_2
+// let userName = prompt('What is your name?');
+// let userAge = prompt('How old are you?');
+// let userLocation = prompt('Where are you from?');
 
-alert(userName + ' ' + userAge + ' ' + userLocation);
-alert(`Your name: ${userName}
-Your age: ${userAge}
-Your city: ${userLocation}`);
+// UNIT_3
+// alert(userName + ' ' + userAge + ' ' + userLocation);
+// alert(`Your name: ${userName}
+// Your age: ${userAge}
+// Your city: ${userLocation}`);
+
+// UNIT_4
+let hours = +prompt('Hours:');
+let seconds = hours * 3600;
+alert(`In ${hours} hours are ${seconds} seconds`);
