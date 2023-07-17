aab9fc5ea27c5a8b490ebbd1a08d4f6039a1e296
[oleksandr_smyrnov] <[oleksandrsmyrnov.work@gmail.com]git config --global user.email [oleksandrsmyrnov.work@gmail.com]>
2023-07-17 23:53:36 +0200

    Add unit_8

--- js/main.js
+++ js/main.js
@@ -1,11 +1,42 @@
-// let userName = prompt('What is your name?');
-// alert(`Hello, ${userName}! How are you?`);
+let arr = prompt('Enter the element of array with comma:').split(','); //Просимо користувача ввести елементи масиву
+alert(`Array is: ${arr}
+Array length is: ${arr.length}`);
+let num = [];
+let str = [];
+for (let i = 0; i < arr.length; i++) {
+  // Якщо елемент масиву число-додаєм в масив чиселбякщо ні-в масив рядків
+  if (!+arr[i]) {
+    str.push(arr[i]);
+  } else {
+    num.push(arr[i]);
+  }
+}
 
-let userName = prompt('What is your name?');
-let userAge = prompt('How old are you?');
-let userLocation = prompt('Where are you from?');
-
-alert(userName + ' ' + userAge + ' ' + userLocation);
-alert(`Your name: ${userName}
-Your age: ${userAge}
-Your city: ${userLocation}`);
+// Перевіряємо чи не пусті масиви
+if (num.lenght == 0) {
+  let sortStr = str.sort();
+  alert(`Sorted array is : ${sortStr}`);
+  let spliceSortStr = sortStr.splice(2, 3);
+  // Виводимо результат
+  alert(
+    `Array after deleting elements from index 2 to 4(including) : ${sortStr}`
+  );
+} else if (str.length == 0) {
+  let sortNum = num.sort((a, b) => a - b);
+  alert(`Sorted array is : ${sortNum}`);
+  let spliceSortNum = sortNum.splice(2, 3);
+  // Виводимо результат
+  alert(
+    `Array after deleting elements from index 2 to 4(including) : ${sortNum}`
+  );
+} else {
+  let sortStr = str.sort();
+  let sortNum = num.sort((a, b) => a - b);
+  let sortArr = [...sortStr, ...sortNum];
+  alert(`Sorted array is : ${sortArr}`);
+  let spliceArr = sortArr.splice(2, 3);
+  // Виводимо результат
+  alert(
+    `Array after deleting elements from index 2 to 4(including) : ${sortArr}`
+  );
+}
