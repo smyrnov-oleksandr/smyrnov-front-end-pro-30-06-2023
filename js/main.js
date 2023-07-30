2a878f749c3a470e761f21ffb8b41c72374776eb
[oleksandr_smyrnov] <[oleksandrsmyrnov.work@gmail.com]git config --global user.email [oleksandrsmyrnov.work@gmail.com]>
2023-07-30 22:22:06 +0200

    Fixed unit_12

--- js/main.js
+++ js/main.js
@@ -5,11 +5,13 @@
 // console.log(array);
 // Результат: [1, 2, 3, 4, 6, 7]
 
-const array = [1, 2, 3, 4, 5, 6, 7];
+const array = [22, 11, 3, 0, 1, 'hi', '33', 0];
+console.log(array);
 
-function removeElement(array, item) {
-  return array.splice(item - 1, 1);
+function removeElement(array, elem) {
+  let res = array.filter((item) => item !== elem);
+  console.log(res);
+  return res;
 }
 
-removeElement(array, 5);
-console.log(array);
+removeElement(array, 0);
