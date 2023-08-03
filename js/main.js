f07e070d0d44715fc44f5b471ec73801e0177e4e
[oleksandr_smyrnov] <[oleksandrsmyrnov.work@gmail.com]git config --global user.email [oleksandrsmyrnov.work@gmail.com]>
2023-08-03 20:56:32 +0200

    Fixed unit_15

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
-// const fibonacci = (n) => {
-//   if (n <= 1) {
-//     return n;
-//   }
-//   let res = fibonacci(n - 1) + fibonacci(n - 2);
-//   console.log(res);
-//   return res;
-// };
-
-// fibonacci(5);
-
-const fib = [0, 1]; // Initialize array!
-let fibNum = 30;
-let out = document.querySelector('p');
-let res = '';
-for (let i = 2; i <= fibNum; i++) {
-  fib[i] = fib[i - 2] + fib[i - 1];
-  res += `${fib[i]}\n`;
+function buildTriangle(rows) {
+  let result = [];
+  for (let row = 0; row < rows; row++) {
+    let rowData = [];
+    for (let col = 0; col <= row; col++) {
+      rowData.push(pascalTriangle(row, col));
+    }
+    result.push(rowData);
+  }
+  return result;
 }
 
-out.innerText = res;
+function pascalTriangle(row, col) {
+  if (col === 0) {
+    return 1;
+  } else if (row === 0) {
+    return 0;
+  } else {
+    return pascalTriangle(row - 1, col) + pascalTriangle(row - 1, col - 1);
+  }
+}
+console.log(buildTriangle(5).join('\n'));
