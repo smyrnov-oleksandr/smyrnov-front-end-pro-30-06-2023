b2280a714ca35886dfc208744d34b9efd6ef1065
[oleksandr_smyrnov] <[oleksandrsmyrnov.work@gmail.com]git config --global user.email [oleksandrsmyrnov.work@gmail.com]>
2023-09-14 19:34:25 +0200

    Added unit_25

--- js/main.js
+++ js/main.js
@@ -1,11 +1,65 @@
-// let userName = prompt('What is your name?');
-// alert(`Hello, ${userName}! How are you?`);
+/**
+ *За допомогою ajax-запиту вивести погоду
 
-let userName = prompt('What is your name?');
-let userAge = prompt('How old are you?');
-let userLocation = prompt('Where are you from?');
+http://api.openweathermap.org/data/2.5/weather?q=LVIV&units=metric&APPID=5d066958a60d315387d9492393935c19
 
-alert(userName + ' ' + userAge + ' ' + userLocation);
-alert(`Your name: ${userName}
-Your age: ${userAge}
-Your city: ${userLocation}`);
+q=XXX - місто, для якого показати погоду
+temp – температура
+pressure - тиск
+description – опис
+humidity – вологість 
+speed – швидкість вітру
+deg - напрям у градусах
+icon - значок, де 10d код іконки
+http://openweathermap.org/img/w/10d.png
+ */
+
+const form = document.getElementById('form');
+const input = document.getElementById('city-name');
+const out = document.getElementById('out');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let cityName = input.value;
+  getWeather(cityName);
+});
+
+function getWeather(cityName) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=c5af3f94362a89a66c25afc9142a2dd1
+    `
+  )
+    .then((response) => {
+      if (!response.ok) {
+        return Promise.reject(new Error(response.statusText));
+      } else {
+        return Promise.resolve(response);
+      }
+    })
+    .then((response) => response.json())
+    .then((data) => renderWeather(data))
+    .catch((error) => {
+      console.log('error', error);
+    });
+}
+
+function renderWeather(data) {
+  console.log(data);
+  out.innerHTML = `
+        <div class="main">
+          <h3>${data.name}</h3>
+          <p>${Math.floor(data.main.temp - 273)} &deg;</p>
+          <img src="https://openweathermap.org/img/wn/${
+            data.weather[0]['icon']
+          }@2x.png">
+          <p>${data.weather[0]['description']}</p>
+        </div>
+        <ul>
+          <li>Pressure: ${data.main.pressure} Pa</li>
+          <li>Humidity: ${data.main.humidity} %</li>
+          <li>Speed: ${data.wind.speed} m/s</li>
+          <li>Deg: ${data.wind.deg} &deg;</li>
+        </ul>
+  `;
+  return out;
+}
